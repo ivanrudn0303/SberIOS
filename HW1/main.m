12e6ffb4cb3ac1b0fe12dfbde65164015997bb2c
ivanrudn0303 <ivanrudn0303@gmail.com>
2019-09-29 13:18:07 +0300

    add HW1

--- HW1/main.m
+++ HW1/main.m
@@ -12,7 +12,7 @@ NSMutableArray<NSNumber*>* testArray = @[@3, @6, @32, @24, @81].mutableCopy;
 NSMutableArray<NSNumber*>* arrayMore20 = [NSMutableArray new];
 NSMutableArray<NSNumber*>* arrayDivBy3 = [NSMutableArray new];
 
-for(uint i = 0; i < testArray.count; i++)
+for (uint i = 0; i < testArray.count; i++)
 {
     if (testArray[i] > 20)
         [arrayMore20 addObject: testArray[i]];
@@ -31,10 +31,13 @@ for(uint i = 0; i < testArray.count; i++)
 }];
 
 // Part2
+NSMutableDictionary* testDict = [NSMutableDictionary new];
 NSString* substring = @"cat";
 NSMutableArray<NSString*>* arrayString = @[@"cataclism", @"caterpillar", @"cat", @"teapot", @"truncate"].mutableCopy;
-for(NSString* item in arrayString)
+for (NSString* item in arrayString)
 {
     if (![item hasPrefix:substring])
         [arrayString removeObject:item];
+    else
+        testDict[item] = @(item.length);
 }
